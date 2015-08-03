8e3afde940209bd4b248797ad2ce36ec53d25c53
Johan <johan@stokking.nl>
2015-08-03 16:54:28 +0200

    Added omitempty flags

--- shared.go
+++ shared.go
@@ -1,19 +1,19 @@
 package shared
 
 type GatewayStatus struct {
-	Time              string   `json:"time"`
-	Latitude          *float64 `json:"latitude"`
-	Longitude         *float64 `json:"longitude"`
-	Altitude          *float64 `json:"altitude"`
-	RxCount           *int     `json:"rxCount"`
-	RxOk              *int     `json:"rxOk"`
-	RxForwarded       *int     `json:"rxForwarded"`
-	AckRatio          *float64 `json:"ackRatio"`
-	DatagramsReceived *int     `json:"datagramsReceived"`
-	DatagramsSent     *int     `json:"datagramsSent"`
+	Time              string   `json:"time,omitempty"`
+	Latitude          *float64 `json:"latitude,omitempty"`
+	Longitude         *float64 `json:"longitude,omitempty"`
+	Altitude          *float64 `json:"altitude,omitempty"`
+	RxCount           *int     `json:"rxCount,omitempty"`
+	RxOk              *int     `json:"rxOk,omitempty"`
+	RxForwarded       *int     `json:"rxForwarded,omitempty"`
+	AckRatio          *float64 `json:"ackRatio,omitempty"`
+	DatagramsReceived *int     `json:"datagramsReceived,omitempty"`
+	DatagramsSent     *int     `json:"datagramsSent,omitempty"`
 }
 
 type RxPacket struct {
-	Time string `json:"time"`
-	Data string `json:"data"`
+	Time string `json:"time,omitempty"`
+	Data string `json:"data,omitempty"`
 }
