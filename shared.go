fa826bec769636232374f22a5c16591722b75d71
Johan <johan@stokking.nl>
2015-08-16 12:47:03 +0200

    Added NodeEui to RxPacket and use built-in time

--- shared.go
+++ shared.go
@@ -1,21 +1,26 @@
 package shared
 
+import (
+	"time"
+)
+
 type GatewayStatus struct {
-	Eui               string   `json:"eui,omitempty"`
-	Time              string   `json:"time,omitempty"`
-	Latitude          *float64 `json:"latitude,omitempty"`
-	Longitude         *float64 `json:"longitude,omitempty"`
-	Altitude          *float64 `json:"altitude,omitempty"`
-	RxCount           *int     `json:"rxCount,omitempty"`
-	RxOk              *int     `json:"rxOk,omitempty"`
-	RxForwarded       *int     `json:"rxForwarded,omitempty"`
-	AckRatio          *float64 `json:"ackRatio,omitempty"`
-	DatagramsReceived *int     `json:"datagramsReceived,omitempty"`
-	DatagramsSent     *int     `json:"datagramsSent,omitempty"`
+	Eui               string    `json:"eui,omitempty"`
+	Time              time.Time `json:"time,omitempty"`
+	Latitude          *float64  `json:"latitude,omitempty"`
+	Longitude         *float64  `json:"longitude,omitempty"`
+	Altitude          *float64  `json:"altitude,omitempty"`
+	RxCount           *int      `json:"rxCount,omitempty"`
+	RxOk              *int      `json:"rxOk,omitempty"`
+	RxForwarded       *int      `json:"rxForwarded,omitempty"`
+	AckRatio          *float64  `json:"ackRatio,omitempty"`
+	DatagramsReceived *int      `json:"datagramsReceived,omitempty"`
+	DatagramsSent     *int      `json:"datagramsSent,omitempty"`
 }
 
 type RxPacket struct {
-	GatewayEui string `json:"gatewayEui,omitempty"`
-	Time       string `json:"time,omitempty"`
-	Data       string `json:"data,omitempty"`
+	GatewayEui string    `json:"gatewayEui,omitempty"`
+	NodeEui    string    `json:"nodeEui,omitempty"`
+	Time       time.Time `json:"time,omitempty"`
+	Data       string    `json:"data,omitempty"`
 }
